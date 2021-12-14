906591ef0dd468bf9c044ce0b0756ec2ae701255
Wallace Breza <wallace.breza@microsoft.com>
2021-12-14 11:44:34 -0800

    Update scripts

--- detect-secrets/scripts/audit.sh
+++ detect-secrets/scripts/audit.sh
@@ -2,12 +2,6 @@
 set -euo pipefail
 
 CURRENT_PATH=$(pwd -P)
-PARENT_PATH=$(
-    cd "$(dirname "${BASH_SOURCE[0]}")"
-    pwd -P
-)
-
-cd "$PARENT_PATH"
 
 source ./activate.sh
 
@@ -15,9 +9,7 @@ check_python
 ensure_env
 
 if which detect-secrets >/dev/null 2>&1; then
-    detect-secrets audit "$CURRENT_PATH/.secrets.baseline"
+    detect-secrets audit .secrets.baseline
 else
     echo "detect-secrets not found. Run setup script and try again"
 fi
-
-cd "$CURRENT_PATH"
--- detect-secrets/scripts/scan.sh
+++ detect-secrets/scripts/scan.sh
@@ -2,24 +2,16 @@
 set -euo pipefail
 
 CURRENT_PATH=$(pwd -P)
-PARENT_PATH=$(
-    cd "$(dirname "${BASH_SOURCE[0]}")"
-    pwd -P
-)
 
-cd "$PARENT_PATH"
-
-source ./activate.sh
+source ./scripts/detect-secrets/activate.sh
 
 check_python
 ensure_env
 
 if which detect-secrets >/dev/null 2>&1; then
     detect-secrets scan \
-        --baseline "$CURRENT_PATH/.secrets.baseline" \
-        --word-list "$CURRENT_PATH/secrets-wordlist.txt"
+        --baseline .secrets.baseline \
+        --word-list secrets-wordlist.txt
 else
     echo "detect-secrets not found. Run setup script and try again"
 fi
-
-cd "$CURRENT_PATH"
