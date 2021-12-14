ec9833a44d7a7045ede280ec0cfcf890552aa8c0
Wallace Breza <wallace.breza@microsoft.com>
2021-12-14 12:25:12 -0800

    Updates internal usage

---
+++ scripts/detect-secrets/activate.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -euo pipefail
+
+CURRENT_PATH=$(pwd -P)
+PYTHON_REF=null
+VENV_PATH=${VIRTUAL_ENV:-}
+
+check_python() {
+    echo "Checking python version..."
+
+    if which python3 >/dev/null 2>&1; then
+        PYTHON_REF="python3"
+    else
+        if which python >/dev/null 2>&1; then
+            PYTHON_REF="python"
+        else
+            echo "Python is not installed.  Install Python 3.8 and try again"
+            exit 1
+        fi
+    fi
+
+    PYTHON_VERSION=$($PYTHON_REF --version 2>&1 | awk '{print $2}')
+    echo "Found Python with version $PYTHON_VERSION"
+}
+
+activate() {
+    VENV_PATH="$CURRENT_PATH/.venv"
+
+    if [[ ! -d "$VENV_PATH" ]]; then
+        echo "Creating environment @ '$VENV_PATH'"
+        $PYTHON_REF -m venv $VENV_PATH
+    fi
+
+    echo "Activating environment @ '$VENV_PATH'"
+    if [ "$(uname)" == "Darwin" ]; then
+        . .venv/bin/activate
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        . .venv/bin/activate
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        . .venv/scripts/activate
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        . .venv/scripts/activate
+    fi
+}
+
+install() {
+    echo 'Installing pre-commit framework...'
+    pip install pre-commit detect-secrets pyahocorasick
+
+    echo 'Installing pre-commit hooks from configuration...'
+    pre-commit install
+    pre-commit run --all-files
+}
+
+ensure_env() {
+    if [[ -z $VENV_PATH ]]; then
+        activate
+        VENV_PATH=${VIRTUAL_ENV:-}
+    fi
+}
---
+++ scripts/detect-secrets/scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+CURRENT_PATH=$(pwd -P)
+PARENT_PATH=$(
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+)
+
+source $PARENT_PATH/activate.sh
+
+check_python
+ensure_env
+
+if which detect-secrets >/dev/null 2>&1; then
+    detect-secrets scan \
+        --baseline .secrets.baseline \
+        --word-list secrets-wordlist.txt
+else
+    echo "detect-secrets not found. Run setup script and try again"
+fi
