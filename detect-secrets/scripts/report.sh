05eb211ca93436d78d2437abe5f204704e95e7a0
Wallace Breza <wallace.breza@microsoft.com>
2021-12-14 14:19:13 -0800

    Add verbose output

--- detect-secrets/scripts/report.sh
+++ detect-secrets/scripts/report.sh
@@ -13,6 +13,10 @@ check_python
 ensure_env
 
 if which detect-secrets >/dev/null 2>&1; then
+    VERSION=$(detect-secrets --version)
+    BASELINE_PATH="$CURRENT_PATH/.secrets.baseline"
+
+    echo "Found detect-secrets with version $VERSION"
     detect-secrets audit --report --only-real .secrets.baseline
 else
     echo "detect-secrets not found. Run setup script and try again"
